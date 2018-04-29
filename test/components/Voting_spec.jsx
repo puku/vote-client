30d5e23abbac5381821fb6afbc605d6218e34631
ivan_levchuk <i.levchuk@itos.eu.org>
2018-04-29 14:49:08 +0300

    Added test to ensure, that Voting component render two buttons. Check label for each button.

--- test/components/Voting_spec.jsx
+++ test/components/Voting_spec.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import ReactTestUtils from 'react-dom/test-utils';
 import Voting from '../../src/components/Voting';
+import {expect} from 'chai';
 
 describe('Voting', () => {
 
@@ -9,6 +10,11 @@ describe('Voting', () => {
     const component = ReactTestUtils.renderIntoDocument(
       <Voting pair={["Trainspotting", "28 Days Later"]} />
     );
+    const buttons = ReactTestUtils.scryRenderedDOMComponentsWithTag(component, 'button');
+
+    expect(buttons.length).to.equal(2);
+    expect(buttons[0].textContent).to.equal('Trainspotting');
+    expect(buttons[1].textContent).to.equal('28 Days Later');
   });
 
 });
