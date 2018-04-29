751ab55b80673a7a9183a65c4d8ec7975bfddc6f
ivan_levchuk <i.levchuk@itos.eu.org>
2018-04-29 14:44:52 +0300

    Modified test_helper.js to run test correctly.

--- test/test_helper.js
+++ test/test_helper.js
@@ -3,8 +3,16 @@ import chai from 'chai';
 import chaiImmutable from 'chai-immutable';
 
 const {JSDOM} = jsdom;
+
 const dom = new JSDOM('<!doctype html><html><body></body></html>');
-const {window} = dom;
-const {document} = dom.window;
+const {win} = dom;
+
+global.window = win;
+
+Object.keys(dom.window).forEach((key) => {
+  if (!(key in global)) {
+    global[key] = dom.window[key];
+  }
+});
 
 chai.use(chaiImmutable);
