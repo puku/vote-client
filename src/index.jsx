38a87ba3d73851c5e2a84e05ffdfae4ed82e13cf
ivan_levchuk <i.levchuk@itos.eu.org>
2018-05-05 19:26:45 +0300

    Updated style, updated npm packages, fixed webpack server config to correctly work with Router

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {BrowserRouter, Route} from 'react-router-dom';
-import {createStore, applyMiddleware} from 'redux';
+import {applyMiddleware, createStore} from 'redux';
 import {Provider} from 'react-redux';
 import io from 'socket.io-client';
 import reducer from './reducer';
@@ -9,7 +9,6 @@ import {setState} from './action_creators';
 import remoteActionMiddleware from './remote_action_middleware';
 import {VotingContainer} from './components/Voting';
 import {ResultsContainer} from './components/Results';
-
 
 const socket = io(`${location.protocol}//${location.hostname}:8090`);
 socket.on('state', state =>
--- webpack.config.js
+++ webpack.config.js
@@ -1,37 +1,42 @@
 module.exports = {
-  mode: "development",
-  entry: [
-    'webpack-dev-server/client?http://localhost:8080',
-    'webpack/hot/only-dev-server',
-    './src/index.jsx'
-  ],
-  module: {
-    rules: [
-      {
-        test: /\.jsx?$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['react']
-          }
-        }
-      },
-      {
-        test: /\.css$/,
-        use: ["style-loader", "css-loader", "postcss-loader"]
-      }
-    ]
-  },
-  resolve: {
-    extensions: ['.js', '.jsx']
-  },
-  output: {
-    path: __dirname + '/dist',
-    publicPath: '/',
-    filename: 'bundle.js'
-  },
-  devServer: {
-    contentBase: './dist'
-  }
+    mode: "development",
+    entry: [
+        'webpack-dev-server/client?http://localhost:8080',
+        'webpack/hot/only-dev-server',
+        './src/index.jsx'
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.jsx?$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['react']
+                    }
+                }
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    {loader: 'css-loader', options: {importLoaders: 1}},
+                    'postcss-loader'
+                ]
+            }
+        ]
+    },
+    resolve: {
+        extensions: ['.js', '.jsx']
+    },
+    output: {
+        path: __dirname + '/dist',
+        publicPath: '/',
+        filename: 'bundle.js'
+    },
+    devServer: {
+        contentBase: './dist',
+        historyApiFallback: true
+    }
 };
